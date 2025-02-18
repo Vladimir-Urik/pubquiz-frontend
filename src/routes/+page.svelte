1c22c98dd93374a00bd7e8c40dd5d31762d1024f
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-02-18 09:50:42 +0100

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 
 </script>
 
-<div class="bg-green-500 w-full h-screen">
-
+<div class="bg-[#785FFF] w-full h-screen flex flex-col justify-center items-center">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-XVY-C6lowF0BWYmoqrT4eRPpeaMogPLTTQ&s" alt="" class="mb-10">
+    <h1 class="uppercase font-bold text-white text-4xl text-center mx-8 mb-14">how chronically online <br> are you?</h1>
+    <button class="text-white font-semibold text-2xl bg-[#b3fc59] rounded-xl w-[40%] border-white border-3 border-b-8 p-y-4">Hrát!</button>
 </div>

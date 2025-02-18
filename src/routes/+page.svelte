89b41fa6bcee3f9c5973e58929dc1e337ddadcf9
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-02-18 10:47:17 +0100

    login, registrace

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,5 @@
 
 <div class="bg-secondary w-full h-screen flex flex-col justify-center items-center">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-XVY-C6lowF0BWYmoqrT4eRPpeaMogPLTTQ&s" alt="" class="mb-10">
-    <h1 class="uppercase font-bold text-white text-4xl text-center mx-8 mb-14">how chronically online <br> are you?</h1>
-    <button class="text-white font-semibold text-2xl bg-[#b3fc59] rounded-xl w-[40%] border-white border-3 border-b-8 p-y-4">Hrát!</button>
-</div>
+    <h1 class="uppercase font-bold text-white text-4xl text-center mx-8 mb-20">how <br> chronically online <br> are you?</h1>
+    <button class="text-white font-semibold text-3xl bg-[#90d63a] rounded-3xl w-[40%] border-white border-3 border-b-8 py-2 hover:bg-[#7fc727]"><a href="/login" target="" rel="noopener noreferrer">Hrát!</a></button></div>
---
+++ src/routes/registrace/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<div class="bg-[#826AFE] w-full h-screen flex flex-col justify-start items-center">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-XVY-C6lowF0BWYmoqrT4eRPpeaMogPLTTQ&s" alt="" class="mb-10">
+    <h1 class="uppercase font-bold text-white text-4xl text-center mx-8 mb-2">sign up</h1>
+    <form action="" class="w-full ">
+        <div class="w-full flex flex-col items-center">
+            <div class="w-[70%] flex flex-col gap-3 mb-5">
+                <input type="text" placeholder="Jméno" class="bg-white border-3 border-black rounded-3xl p-2 w-full">
+                <input type="email" placeholder="Email" class="bg-white border-3 border-black rounded-3xl p-2 w-full">
+                <input type="password" placeholder="Heslo" class="bg-white border-3 border-black rounded-3xl p-2 w-full"> 
+            </div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTX8xxZOb2HrwQ8zksLn8Tv-Pz36PK9PkmTA&s" alt="">
+        </div>
+    </form>
+    <button class="text-white font-semibold text-3xl bg-[#90d63a] rounded-3xl w-[40%] border-white border-3 border-b-8 py-2 hover:bg-[#7fc727] mt-10"><a href="/" target="" rel="noopener noreferrer">Done</a></button>
+</div>

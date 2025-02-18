c2b50e5884e54da54def46bbdb00255874e599bf
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-02-18 10:56:37 +0100

    update barvicky, vytvoreni quiz page

--- src/routes/registrace/+page.svelte
+++ src/routes/registrace/+page.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<div class="bg-[#826AFE] w-full h-screen flex flex-col justify-start items-center">
+<div class="bg-secondary w-full h-screen flex flex-col justify-start items-center">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-XVY-C6lowF0BWYmoqrT4eRPpeaMogPLTTQ&s" alt="" class="mb-10">
     <h1 class="uppercase font-bold text-white text-4xl text-center mx-8 mb-2">sign up</h1>
     <form action="" class="w-full ">
@@ -15,5 +15,5 @@
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTX8xxZOb2HrwQ8zksLn8Tv-Pz36PK9PkmTA&s" alt="">
         </div>
     </form>
-    <button class="text-white font-semibold text-3xl bg-[#90d63a] rounded-3xl w-[40%] border-white border-3 border-b-8 py-2 hover:bg-[#7fc727] mt-10"><a href="/" target="" rel="noopener noreferrer">Done</a></button>
+    <button class="text-white font-semibold text-3xl bg-primary rounded-3xl w-[40%] border-white border-3 border-b-8 py-2 hover:bg-primary-600 mt-10"><a href="/" target="" rel="noopener noreferrer">Done</a></button>
 </div>

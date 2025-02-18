29f6644edf5729743c5bc38a72d368f898ad3dcc
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-02-18 11:09:12 +0100

    quiz

---
+++ src/routes/quiz/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<div class="bg-secondary">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-XVY-C6lowF0BWYmoqrT4eRPpeaMogPLTTQ&s" alt="" class="mb-10 h-25 w-auto">
+
+<div class=" w-full h-screen flex flex-col items-center gap-5">
+    <button class="text-white font-semibold text-4xl bg-[#90d63a] rounded-full w-[60%] border-white border-3 border-b-15 py-2 hover:bg-[#7fc727]"><a href="/" target="" rel="noopener noreferrer">Slang</a></button>
+    <button class="text-white font-semibold text-4xl bg-[#90d63a] rounded-full w-[60%] border-white border-3 border-b-15 py-2 hover:bg-[#7fc727]"><a href="/" target="" rel="noopener noreferrer">Celebrity</a></button>
+    <button class="text-white font-semibold text-4xl bg-[#90d63a] rounded-full w-[60%] border-white border-3 border-b-15 py-2 hover:bg-[#7fc727]"><a href="/" target="" rel="noopener noreferrer">Anime</a></button>
+    <button class="text-white font-semibold text-4xl bg-[#90d63a] rounded-full w-[60%] border-white border-3 border-b-15 py-2 hover:bg-[#7fc727]"><a href="/" target="" rel="noopener noreferrer">Emoji</a></button>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTX8xxZOb2HrwQ8zksLn8Tv-Pz36PK9PkmTA&s" alt="">
+</div>
+
+<!--menu komponent zde-->
+</div>

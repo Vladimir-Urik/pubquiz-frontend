ba40cca4d3c6819f77a09bda0b2bafda4987686d
vitis22 <vitschneider@icloud.com>
2025-02-19 11:07:20 +0100

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,17 @@
-<script>
+<script lang="ts">
     export let error;
-    let status = error?.status || 500;
+    let status = error?.status;
+
+    import Button from "../components/Button.svelte";
+    import Heading from "../components/Heading.svelte";
+    import Container from '../components/Container.svelte';
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-12">
-    <h1 class="text-5xl font-bold text-red-500">{status} - Chyba</h1>
-    <p class="text-xl mt-4">{error?.message || 'Něco se pokazilo...'}</p>
-    <a href="/" class="mt-6 px-6 py-3 bg-blue-500 text-white text-lg rounded-lg shadow-md hover:bg-blue-600 transition">
-        Přejít na hlavní stránku
-    </a>
-</main>
+<Container>
+  
+    <Heading text={status}Jejda />
+
+    <Heading text={error?.message || 'Něco se pokazilo...'} />
+   <Button href="/" text="Domů" />
+
+</Container>

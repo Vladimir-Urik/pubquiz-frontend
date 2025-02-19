832a548b2cb86b08fe5464b0042376d32c575213
vitis22 <vitschneider@icloud.com>
2025-02-19 12:24:58 +0100

    Update Button.svelte

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    export let href: string = "";
-    export let text: string;
-    export let target: string = "";
+    // Vstupní proměnné pro komponentu
+    export let href: string = "";  // Cílový odkaz
+    export let text: string;  // Text odkazu
+    export let target: string = "";  // Atribut target (_blank, _self atd.)
 </script>
 
+<!-- Odkazové tlačítko s dynamickými vlastnostmi -->
 <a
-    href="{href}"
+    href="{href}"  
     class="text-white text-center font-semibold text-3xl rounded-3xl px-8 border-black bg-primary button hover:bg-primary-600 border-3 border-b-8 py-2 transition-colors duration-200"
-    {target}
-    rel="noopener noreferrer"
+    {target}  
+    rel="noopener noreferrer" 
     on:click
 >
-    {text}
+    {text} 
 </a>

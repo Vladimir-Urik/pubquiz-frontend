a357fb43db2e16c6c5b610fac0f35c91a682345f
vitis22 <vitschneider@icloud.com>
2025-02-19 12:24:48 +0100

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    export let error;
-    let status = error?.status;
+    export let error; // Proměnná pro uchování chybové zprávy (přijímá data zvenčí)
+    let status = error?.status; // Získání stavového kódu z objektu error (pokud existuje)
 
+    // Import komponent použitých v této stránce
     import Button from "../components/Button.svelte";
     import Heading from "../components/Heading.svelte";
     import Container from '../components/Container.svelte';
@@ -9,9 +10,9 @@
 
 <Container>
   
-    <Heading text={status}Jejda />
+    <Heading text={status}Jejda /> <!-- Nadpis s chybovou zprávou -->
+    <Heading text={error?.message || 'Něco se pokazilo...'} /> <!-- Nadpis s chybovou zprávou -->
 
-    <Heading text={error?.message || 'Něco se pokazilo...'} />
-   <Button href="/" text="Domů" />
+   <Button href="/" text="Domů" /> <!-- Tlačítko pro návrat domů -->
 
 </Container>

29ace110eb489d676912203d847111451d9d6376
vitis22 <vitschneider@icloud.com>
2025-02-19 12:59:06 +0100

    Update Logo.svelte

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-    export let imgSrc = "/logo.svg";
+    // Exportování proměnné 'imgSrc' pro specifikaci cesty k obrázku (výchozí hodnota je '/logo.svg')
+    export let imgSrc = "/logo.svg"; 
+    // Exportování proměnné 'altText' pro alternativní text k obrázku (výchozí hodnota je prázdný řetězec)
     export let altText = "";
 </script>
 
-<img src={imgSrc} alt={altText} class="w-[75%] max-w-[400px] mb-14" />
+<!-- Obrázek s dynamicky nastavenými atributy 'src' a 'alt' -->
+<img 
+    src={imgSrc}        
+    alt={altText}       
+    class="w-[75%] max-w-[400px] mb-14"  
+/>

5976c2c93d8a1382f336df80eba2196a940f8f43
vitis22 <vitschneider@icloud.com>
2025-02-19 12:58:13 +0100

    Update Input.svelte

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-    export let type: string = "text";
+    // Exportování proměnné 'type' pro definování typu vstupního pole (výchozí je 'text')
+    export let type: string = "text"; 
+    // Exportování proměnné 'placeholder' pro zobrazení textu v poli, když je prázdné
     export let placeholder: string;
+    // Exportování proměnné 'value' pro uložení hodnoty zadané uživatelem (výchozí je prázdný řetězec)
     export let value: string = "";
 </script>
 
+<!-- Vstupní pole, které používá proměnné pro 'type', 'placeholder' a 'value' -->
 <input
-    {type}
-    {placeholder}
-    bind:value
-    class="bg-white border-3 border-black rounded-3xl py-2 px-4 w-full"
+    {type}         
+    {placeholder}  
+    bind:value    
+    class="bg-white border-3 border-black rounded-3xl py-2 px-4 w-full" 
 />

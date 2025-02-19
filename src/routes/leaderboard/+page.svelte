0257f94337f69331fbf770066e293f8854d6924f
Michaela Ptáčková <michaela.ptackova.mp@gmail.com>
2025-02-19 15:51:59 +0100

    leaderboard page layout

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Container from "../../components/Container.svelte";
+    import SmLogo from "../../components/smLogo.svelte";
+    import Menu from "../../components/Menu.svelte";
+
+</script>
+
+<div class="bg-secondary p-5 h-[100dvh]">
+    <SmLogo/>
+
+    <div class="w-full flex flex-col items-center gap-7 xl:flex-row-reverse xl:justify-center xl:items-start xl:mt-40">
+        <div class="flex flex-row gap-5 items-end">
+            <img src="https://files.ekmcdn.com/westbridgford/images/galaxy-1st-2nd-3rd-place-medal-45mm-438295-p.jpg" alt="" class="h-20">
+            <img src="https://files.ekmcdn.com/westbridgford/images/galaxy-1st-2nd-3rd-place-medal-45mm-438295-p.jpg" alt="" class="h-40">
+            <img src="https://files.ekmcdn.com/westbridgford/images/galaxy-1st-2nd-3rd-place-medal-45mm-438295-p.jpg" alt="" class="h-20">
+
+        </div>
+        
+    </div>
+        <div class="h-[50%] bg-white w-full mt-8 rounded-3xl">
+
+        </div>
+   <Menu/>
+</div>

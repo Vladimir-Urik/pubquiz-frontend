a6c6be8bd01f1a4934b21175f45e3bc1bcd8f598
vitis22 <vitschneider@icloud.com>
2025-02-19 12:38:35 +0100

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,44 @@
 <script lang="ts">
-    import Container from "../../components/Container.svelte";
-    import Input from "../../components/Input.svelte";
-    import Button from "../../components/Button.svelte";
-    import Heading from "../../components/Heading.svelte";
-    import Logo from '../../components/Logo.svelte';
+    // Importování komponent používaných v této stránce
+    import Container from "../../components/Container.svelte"; // Hlavní obalovací kontejner
+    import Input from "../../components/Input.svelte"; // Vstupní pole pro formulář
+    import Button from "../../components/Button.svelte"; // Tlačítko
+    import Heading from "../../components/Heading.svelte"; // Nadpis
+    import Logo from '../../components/Logo.svelte'; // Logo aplikace
 
-    let username = "";
-    let password = "";
+    // Proměnné pro ukládání hodnot z formuláře
+    let username = ""; // Uloží zadané jméno
+    let password = ""; // Uloží zadané heslo
 </script>
 
 <Container>
+    <!-- Logo aplikace -->
     <Logo />
+    
+    <!-- Nadpis stránky -->
     <Heading text="Přihlášení" />
+
+    <!-- Formulář pro přihlášení -->
     <form action="" class="w-full">
         <div class="w-full flex flex-col items-center">
             <div class="w-[70%] md:max-w-[300px] flex flex-col gap-3 mb-5">
+                <!-- Vstupní pole pro uživatelské jméno -->
                 <Input type="text" placeholder="Jméno" bind:value={username} />
+                
+                <!-- Vstupní pole pro heslo -->
                 <Input type="password" placeholder="Heslo" bind:value={password} />
-                <p class="text-white justify-end text-center">Nemáš účet? <a href="/register" class="underline decoration-2 decoration-primary underline-offset-2">Registrovat se</a></p>
+                
+                <!-- Odkaz na registraci, pokud uživatel nemá účet -->
+                <p class="text-white justify-end text-center">
+                    Nemáš účet? 
+                    <a href="/register" class="underline decoration-2 decoration-primary underline-offset-2">
+                        Registrovat se
+                    </a>
+                </p>
             </div>
         </div>
     </form>
+
+    <!-- Tlačítko pro odeslání formuláře -->
     <Button href="/" text="Odeslat" />
 </Container>

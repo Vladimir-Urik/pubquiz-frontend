a3fcb27f43f25e1716d9fa201861dab2fa94ec1d
vitis22 <vitschneider@icloud.com>
2025-02-19 12:34:08 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-    import Button from "../components/Button.svelte";
-    import Logo from "../components/Logo.svelte";
-    import Heading from "../components/Heading.svelte";
-    import Container from '../components/Container.svelte';
+    // Import komponent použitých v této stránce
+    import Button from "../components/Button.svelte";   // Tlačítko s odkazem
+    import Logo from "../components/Logo.svelte";       // Logo aplikace
+    import Heading from "../components/Heading.svelte"; // Nadpis
+    import Container from '../components/Container.svelte'; // Kontejner pro zarovnání obsahu
 </script>
 
 <Container>
+    <!-- Logo aplikace -->
     <Logo />
+
+    <!-- Nadpis s textem (řádky jsou oddělené pomocí \n) -->
     <Heading text="how\nchronically online\nare you?" />
+
+    <!-- Tlačítko pro přechod na stránku přihlášení -->
     <Button href="/login" text="Hrát!" />
 </Container>

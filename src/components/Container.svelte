a4b62f6739c4c59ad46665aa1d56bf37a1956ea3
vitis22 <vitschneider@icloud.com>
2025-02-19 12:41:52 +0100

    Update Container.svelte

--- src/components/Container.svelte
+++ src/components/Container.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+    // Exportování proměnné className s výchozí hodnotou pro nastavení tříd CSS
     export let className = "bg-secondary py-8 w-full min-h-fit h-[100dvh] flex flex-col justify-center items-center";
+    // className obsahuje definice pro barvu pozadí, padding, šířku a výšku, a flexibilní rozložení
 </script>
 
+<!-- Vytváří div s dynamicky přiřazenými třídami CSS -->
 <div class={className}>
+    <!-- Slot pro vkládání obsahu, který bude použitý uvnitř tohoto komponentu -->
     <slot />
 </div>

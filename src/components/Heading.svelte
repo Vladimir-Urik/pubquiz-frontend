7a96034a8cb9602ad48b6d5f301ffd5bfc28a567
vitis22 <vitschneider@icloud.com>
2025-02-19 12:42:52 +0100

    Update Heading.svelte

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-    export let text = "";
+    // Exportování proměnné text pro zobrazení textu v komponentě
+    export let text = ""; 
+    // Exportování proměnné className s výchozími CSS třídami pro stylování nadpisu
     export let className = "uppercase font-black text-white text-4xl md:text-5xl text-center mx-8 mb-10";
+    // className obsahuje nastavení pro velká písmena, tučný font, bílou barvu textu, velikost písma, zarovnání na střed a okraje
 </script>
 
-<h1 class={className}>{@html text.replace(/\\n/g, '<br>')}</h1>
+<!-- Nadpis, který používá className pro styly a zobrazuje text s náhradou znaků pro nový řádek -->
+<h1 class={className}>
+    <!-- Nahrazuje "\n" znak za HTML <br> pro přidání nových řádků -->
+    {@html text.replace(/\\n/g, '<br>')}
+</h1>
